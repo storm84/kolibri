272b2bc9c13bcc3ee92cab23566afd23b73f8c60
Marcus Storm <storm.marcus@hotmail.com>
2023-12-15 00:12:42 +0100

    feat: add seed data

--- src/Kolibri.Api/Extensions/ConfigureApplicationExtension.cs
+++ src/Kolibri.Api/Extensions/ConfigureApplicationExtension.cs
@@ -1,3 +1,6 @@
+using Kolibri.Api.Models;
+using Kolibri.Api.Repositories;
+
 namespace Kolibri.Api.Extensions;
 
 public static class ConfigureApplicationExtension
@@ -10,10 +13,26 @@ public static class ConfigureApplicationExtension
             app.UseSwaggerUI();
         }
 
+        SeedData(app);
+
         app.UseHttpsRedirection();
 
         app.MapControllers();
 
         return app;
+    }
+
+    private static void SeedData(WebApplication app)
+    {
+        var packageRepository = app.Services.GetRequiredService<IPackageRepository>();
+        var packages = new List<Package>{
+            new Package("999111111111111111", 2000, 30, 40, 50),
+            new Package("999222222222222222", 2000, 300, 40, 50)
+        };
+
+        foreach (var package in packages)
+        {
+            packageRepository.AddPackage(package);
+        }
     }
 }
